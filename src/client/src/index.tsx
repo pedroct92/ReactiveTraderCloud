db034e2327e4cc2cc5299fec368f4e78c665652f
Evan Schneider <me@evanrs.com>
2018-10-09 11:30:03 -0400

    revet me. really. I'm not supposed to be here, but here we are 🤷

--- src/client/src/index.tsx
+++ src/client/src/index.tsx
@@ -5,16 +5,17 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 // Side-effecting import for rt-theme/globals
 import 'rt-theme'
 
-import { MainRoute, NotificationRoute, OrderTicketRoute, SimpleLauncher, StyleguideRoute } from './routes'
+// import { MainRoute, NotificationRoute, OrderTicketRoute, SimpleLauncher, StyleguideRoute } from './routes'
+import { OrderTicketRoute } from './routes/OrderTicketRoute'
+import { StyleguideRoute } from './routes/StyleguideRoute'
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/launcher" component={SimpleLauncher} />
+      {/* <Route path="/launcher" component={SimpleLauncher} /> */}
       <Route path="/styleguide" component={StyleguideRoute} />
       <Route path="/order-ticket" component={OrderTicketRoute} />
-      <Route path="/notification" component={NotificationRoute} />
-      <Route component={MainRoute} />
+      <Route path="/" component={OrderTicketRoute} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root'),
