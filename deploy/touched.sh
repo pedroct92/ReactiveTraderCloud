721614b4b626a34d21333a8ed12c7b5aa038ac0e
Evan Schneider <me@evanrs.com>
2018-10-10 13:59:23 -0400

    Report modified files

--- deploy/touched.sh
+++ deploy/touched.sh
@@ -2,18 +2,19 @@
 
 set -e
 
-BRANCH=${CIRCLE_BRANCH:-"develop"}
-
-COMPARE_FROM=$(git rev-parse $BRANCH)
-if [[ $(echo $BRANCH | grep -E "(master|develop)") != "" ]]; then
-    COMPARE_FROM="@"
+TARGET=develop
+COMMIT=$(git rev-parse $TARGET)
+if [[ $(echo $CIRCLE_BRANCH | grep -E "(master|develop)") != "" ]]; then
+    COMMIT="@"
 fi;
+
+echo "Comparing from $TARGET $COMMIT"
 
 export BUILD_LIST=()
 export LOAD_LIST=()
 export RUN_LIST=()
 export PULL_LIST=()
-export TOUCHED_FILES=$(git diff --name-only --diff-filter=ADMR $COMPARE_FROM~..@)
+export TOUCHED_FILES=$(git diff --name-only --diff-filter=ADMR $COMMIT~..@)
 
 [[ $(echo $TOUCHED_FILES | grep "^src/server") != "" ]] && \
     export SERVER_TOUCHED=1 || export SERVER_TOUCHED=0
